2473c6e2c2497a46282188b77241bea17163437b
Vasya Lesiuk <lenono526@gmail.com>
2020-11-17 10:46:13 +0200

    Добавьте файлы проекта.

---
+++ PointLibrary/ShapePoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+
+
+namespace PointLibrary
+{
+    public struct pictureData
+    {
+        public int Point1X { get; set; }
+        public int Point1Y { get; set; }
+        public int Point2X { get; set; }
+        public int Point2Y { get; set; }
+        public int Point3X { get; set; }
+        public int Point3Y { get; set; }
+    }
+    public class ShapePoint
+    {
+
+        protected pictureData _point;
+        public ShapePoint()
+        {
+
+        }
+        public virtual void drow(Grid grid)
+        {
+
+        }
+    }
+}
